25a3a1be53430b29bd19c088a08e6d55227c5cb4
ThijmenJava <94443778+ThijmenJava@users.noreply.github.com>
2022-05-03 00:53:27 +0200

    test

---
+++ build/CMakeFiles/sft.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sft.dir/main.cpp.obj"
+  "CMakeFiles/sft.dir/main.cpp.obj.d"
+  "libsft.dll.a"
+  "sft.exe"
+  "sft.exe.manifest"
+  "sft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/c projects/sft-Structure-File-Type-
+# Build directory: C:/c projects/sft-Structure-File-Type-/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
